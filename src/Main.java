25f00eea47006a7a308721cf29dc12f62a2a3f39
solpi2 <parkchini@gmail.com>
2025-06-05 09:38:04 +0900

    continue. 369

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i =1; i <= 10; i++) {
-            if (i == 7) {
-                break;
+        for (int i = 1; i <= 10; i++) {
+            if (i % 3 != 0) {
+                continue;
+
             }
 
             System.out.println(i);
