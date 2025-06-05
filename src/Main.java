9b267520d59fbfc9b5b6f0a9002c57e1428c6dc5
solpi2 <parkchini@gmail.com>
2025-06-05 10:35:06 +0900

    continue,break

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            if (i % 3 != 0) {
+        for (int i = 1; i <= 20; i++) {
+            if (i % 5 == 0) {
                 continue;
-
             }
+            if (i >= 15)
+                break;
 
             System.out.println(i);
         }
